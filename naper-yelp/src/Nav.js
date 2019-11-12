5250ff9e4efe057d3ccbdf1779d90dbe6396eb7f
BETeng <brian7.teng@gmail.com>
2019-11-11 19:19:39 -0600

    added react router and nav results page

---
+++ naper-yelp/src/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function Nav() {
+  return (
+    <nav>
+      <h1> ~ Logo ~</h1>
+      <ul>
+        <li>Home (search) </li>
+        <li>Results </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
---
+++ naper-yelp/src/Results.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Results() {
+  return (
+    <div className="Results">
+      <h1>Results Page</h1>
+    </div>
+  );
+}
+
+export default Results;
