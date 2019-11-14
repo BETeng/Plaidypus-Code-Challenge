9728262cf4c25e513f6df301b4c6b0dee78e5310
BETeng <brian7.teng@gmail.com>
2019-11-14 13:39:48 -0600

    fetch search works

--- naper-yelp/src/Search.js
+++ naper-yelp/src/Search.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
 
+
 function Search() {
-  
 const apiKey = process.env.REACT_APP_API_KEY;
 const headerAuth = "Bearer "+apiKey;
 console.log(headerAuth);
+var apiTerm;
+var apiLocation;
+var apiEndpoint = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=chipotle&location=60540&limit=5";
+
+fetch(apiEndpoint, {
+  method: "GET",
+  headers: {
+    "Authorization": `Bearer ${apiKey}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
 
   return (
     <div>
