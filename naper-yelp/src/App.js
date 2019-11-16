f12e5650fbc28f23ecb8f36554a9c5e6e6ee0177
BETeng <brian7.teng@gmail.com>
2019-11-16 00:16:26 -0600

    got the api to work with componentdidmount

--- naper-yelp/src/App.js
+++ naper-yelp/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from './Nav';
 import Results from './Results';
 import Search from "./Search";
--- naper-yelp/src/Search.js
+++ naper-yelp/src/Search.js
@@ -1,30 +1,97 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
+class Search extends Component {
 
-function Search() {
-const apiKey = process.env.REACT_APP_API_KEY;
-const headerAuth = "Bearer "+apiKey;
-console.log(headerAuth);
-var apiTerm;
-var apiLocation;
-var apiEndpoint = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=chipotle&location=60540&limit=5";
-
-fetch(apiEndpoint, {
-  method: "GET",
-  headers: {
-    "Authorization": `Bearer ${apiKey}`
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
   }
-})
-  .then(res => res.json())
-  .then(json => console.log(json));
 
-  return (
-    <div>
-      <h1>Search Page</h1>
-    </div>
+  componentDidMount() {
 
-  );
+    const apiKey = process.env.REACT_APP_API_KEY;
+    const headerAuth = "Bearer " + apiKey;
+    console.log(headerAuth);
+    var apiTerm;
+    var apiLocation;
+    var apiEndpoint = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=mexican&location=naperville&limit=10";
+
+    fetch(apiEndpoint, {
+      method: "GET",
+      headers: {
+        "Authorization": `Bearer ${apiKey}`
+      }
+    })
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json.businesses,
+        })
+      });
+  }
+
+  render() {
+
+    var { isLoaded, items } = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    }
+
+    else {
+
+      return (
+       
+      
+        <div className="Search">
+          <ul>
+            {items.map(item => (
+              <li key = {item.id}>
+                {item.name}  
+              </li>
+            ))};
+          </ul>
+        </div>
+      )
+    }
+  }
+
 }
-
 export default Search;
+
+
+// import React from 'react';
+// import './App.css';
+
+
+// function Search() {
+// const apiKey = process.env.REACT_APP_API_KEY;
+// const headerAuth = "Bearer "+apiKey;
+// console.log(headerAuth);
+// var apiTerm;
+// var apiLocation;
+// var apiEndpoint = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=chipotle&location=60540&limit=5";
+
+// fetch(apiEndpoint, {
+//   method: "GET",
+//   headers: {
+//     "Authorization": `Bearer ${apiKey}`
+//   }
+// })
+//   .then(res => res.json())
+//   .then(json => console.log(json.businesses));
+
+//   return (
+//     <div>
+//       <h1>Search Page</h1>
+//     </div>
+
+//   );
+// }
+
+// export default Search;
